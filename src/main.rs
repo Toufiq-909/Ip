c9b2137600dfbdcacfcd5cd6d410e450584eb7ba
Toufiq-909 <muheeth312@gmail.com>
2025-09-30 18:31:23 +0530

    response

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ use tower_http::cors::{CorsLayer,Any};
  async fn main()
  {
     dotenv().ok();
-    let cors=CorsLayer::new().allow_origin(Any);  
+    let cors=CorsLayer::new().allow_origin(Any).allow_headers(Any);  
     let app=Router::new().route("/getip",get(Find)).layer(cors);
     let listener=tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener,app.into_make_service_with_connect_info::<SocketAddr>()).await.unwrap();
